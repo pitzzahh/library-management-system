afee8146df273d0a8cc1bc9b96217b457b3bc4ed
pitzzahh <araopj2191@gmail.com>
2022-11-29 15:15:26 +0800

    add enum value

--- src/main/java/io/github/pitzzahh/libraryManagementSystem/entity/Course.java
+++ src/main/java/io/github/pitzzahh/libraryManagementSystem/entity/Course.java
@@ -1,10 +1,20 @@
 package io.github.pitzzahh.libraryManagementSystem.entity;
 
 public enum Course {
-    BSIT,
-    BSHM,
-    BSBA,
-    BSCS,
-    BSCPE
+    BSIT("Bachelor of Science in Information Technology"),
+    BSHM("Bachelor of Science in Hotel Management"),
+    BSBA("Bachelor of Science in Business Administration"),
+    BSCS("Bachelor of Science in Computer Science"),
+    BSCPE("Bachelor of Science in Computer Engineering"),
+    BSAIS("Bachelor of Science in Accountancy Information System");
 
+    private String courseName;
+
+    Course(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
 }
