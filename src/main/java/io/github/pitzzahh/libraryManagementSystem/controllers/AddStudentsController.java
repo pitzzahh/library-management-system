d752efdd93b63cd9003a96e61b6e8efbec3c1f31
pitzzahh <pitzzahh@gmail.com>
2022-11-30 15:09:04 +0800

    added functionality on adding student details on studentTable (with tooltips)

--- src/main/java/io/github/pitzzahh/libraryManagementSystem/controllers/AddStudentsController.java
+++ src/main/java/io/github/pitzzahh/libraryManagementSystem/controllers/AddStudentsController.java
@@ -7,7 +7,6 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.collections.ObservableList;
 import javafx.collections.FXCollections;
 import javafx.scene.input.MouseEvent;
-import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.util.Duration;
 import javafx.fxml.FXML;
@@ -44,62 +43,124 @@ public class AddStudentsController {
     // TODO: move to Util interface
     private final ObservableList<Student> dataSource = FXCollections.observableArrayList();
 
+    private boolean passed;
+
     @FXML
-    public void onAddStudent(ActionEvent actionEvent) {
-        actionEvent.consume();
-        dataSource.add(new Student(generateRandomAccountNumber(), "John", "Doe", Course.BSIT));
-        TableColumn<Student, ?> studentNumberColumn = studentTable.getColumns().get(0);
-        studentNumberColumn.setStyle("-fx-alignment: CENTER;");
-        studentNumberColumn.setCellValueFactory(new PropertyValueFactory<>("studentNumber"));
-        studentTable.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("firstName"));
-        studentTable.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("lastName"));
-        TableColumn<Student, ?> studentCourseColumn = studentTable.getColumns().get(3);
-        studentCourseColumn.setStyle("-fx-alignment: CENTER;");
-        studentCourseColumn.setCellValueFactory(new PropertyValueFactory<>("course"));
-        studentTable.setItems(dataSource);
+    public void onAddStudent(MouseEvent event) {
+        if (passed) {
+            if (!isStudentAlreadyAdded(studentTable, studentId.getText().trim())) {
+                dataSource.add(new Student(
+                        studentId.getText().trim(),
+                        firstName.getText().trim(),
+                        lastName.getText().trim(),
+                        course.getSelectionModel().getSelectedItem()
+                ));
+
+                TableColumn<Student, ?> studentNumberColumn = studentTable.getColumns().get(0);
+                studentNumberColumn.setStyle("-fx-alignment: CENTER;");
+                studentNumberColumn.setCellValueFactory(new PropertyValueFactory<>("studentNumber"));
+
+                studentTable.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("firstName"));
+
+                studentTable.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("lastName"));
+
+                TableColumn<Student, ?> studentCourseColumn = studentTable.getColumns().get(3);
+                studentCourseColumn.setStyle("-fx-alignment: CENTER;");
+                studentCourseColumn.setCellValueFactory(new PropertyValueFactory<>("course"));
+
+                studentTable.setItems(dataSource);
+            }
+            else {
+                Tooltip tooltip = initToolTip(
+                        "Cannot add student, Student with student number already added",
+                        event,
+                        adminButtonFunctionsToolTipStyle()
+                );
+                tooltip.setAutoHide(true);
+                tooltip.setShowDuration(Duration.seconds(3));
+                addStudentTable.setTooltip(tooltip);
+                addStudentTable.getTooltip().show(getParent("add_students_window").getScene().getWindow());
+            }
+        }
     }
 
     @FXML
     public void onHoverAddStudent(MouseEvent event) {
-        Tooltip tooltip = initToolTip(
-                "Add Student",
+        passed = checkInputs(
+                addStudentTable,
                 event,
-                adminButtonFunctionsToolTipStyle()
+                studentId.getText().trim(),
+                firstName.getText().trim(),
+                lastName.getText().trim()
         );
-        tooltip.setShowDuration(Duration.seconds(3));
-        addStudentTable.setTooltip(tooltip);
+        if (passed) {
+            Tooltip tooltip = initToolTip(
+                    "Add Student",
+                    event,
+                    adminButtonFunctionsToolTipStyle()
+            );
+            tooltip.setShowDuration(Duration.seconds(3));
+            addStudentTable.setTooltip(tooltip);
+        }
     }
 
     @FXML
     public void onHoverRemoveStudent(MouseEvent event) {
-        Tooltip tooltip = initToolTip(
-                "Remove Student",
+        passed = checkInputs(
+                addStudentTable,
                 event,
-                adminButtonFunctionsToolTipStyle()
+                studentId.getText().trim(),
+                firstName.getText().trim(),
+                lastName.getText().trim()
         );
-        tooltip.setShowDuration(Duration.seconds(3));
-        removeStudentsTable.setTooltip(tooltip);
+        if (passed) {
+            Tooltip tooltip = initToolTip(
+                    "Remove Student",
+                    event,
+                    adminButtonFunctionsToolTipStyle()
+            );
+            tooltip.setShowDuration(Duration.seconds(3));
+            removeStudentsTable.setTooltip(tooltip);
+        }
     }
 
     @FXML
     public void onHoverRemoveAll(MouseEvent event) {
-        Tooltip tooltip = initToolTip(
-                "Remove All Students records from the table",
+        passed = checkInputs(
+                addStudentTable,
                 event,
-                adminButtonFunctionsToolTipStyle()
+                studentId.getText().trim(),
+                firstName.getText().trim(),
+                lastName.getText().trim()
         );
-        tooltip.setShowDuration(Duration.seconds(3));
-        removeAllStudentsTable.setTooltip(tooltip);
+        if (passed) {
+            Tooltip tooltip = initToolTip(
+                    "Remove All Students records from the table",
+                    event,
+                    adminButtonFunctionsToolTipStyle()
+            );
+            tooltip.setShowDuration(Duration.seconds(3));
+            removeAllStudentsTable.setTooltip(tooltip);
+        }
     }
 
     @FXML
     public void onHoverSaveAll(MouseEvent event) {
-        Tooltip tooltip = initToolTip(
-                "Save all students records from the table to the database",
+        passed = checkInputs(
+                addStudentTable,
                 event,
-                adminButtonFunctionsToolTipStyle()
+                studentId.getText().trim(),
+                firstName.getText().trim(),
+                lastName.getText().trim()
         );
-        tooltip.setShowDuration(Duration.seconds(3));
-        saveALlStudentsTable.setTooltip(tooltip);
+        if (passed) {
+            Tooltip tooltip = initToolTip(
+                    "Save all students records from the table to the database",
+                    event,
+                    adminButtonFunctionsToolTipStyle()
+            );
+            tooltip.setShowDuration(Duration.seconds(3));
+            saveALlStudentsTable.setTooltip(tooltip);
+        }
     }
 }
