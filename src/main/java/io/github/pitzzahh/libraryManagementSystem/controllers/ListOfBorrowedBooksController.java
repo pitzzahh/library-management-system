9ae41309f78c020e0e1b6d7ba79f17624e6ba630
pitzzahh <pitzzahh@gmail.com>
2022-12-04 16:25:27 +0800

    removed unused initialize method

--- src/main/java/io/github/pitzzahh/libraryManagementSystem/controllers/ListOfBorrowedBooksController.java
+++ src/main/java/io/github/pitzzahh/libraryManagementSystem/controllers/ListOfBorrowedBooksController.java
@@ -3,22 +3,14 @@ package io.github.pitzzahh.libraryManagementSystem.controllers;
 import io.github.pitzzahh.libraryManagementSystem.entity.Book;
 import javafx.scene.control.TableView;
 import javafx.scene.control.Button;
-import javafx.fxml.Initializable;
-import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import java.net.URL;
 
-public class ListOfBorrowedBooksController implements Initializable {
+public class ListOfBorrowedBooksController {
 
     @FXML
     public Button separateWindowButton;
 
-
     @FXML
     public TableView<Book> table;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
-    }
 }
