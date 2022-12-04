09b0f52d8686e276196ed9522b9bb5f11dfd092d
pitzzahh <pitzzahh@gmail.com>
2022-12-04 18:19:08 +0800

    refactored adding data to available books table

--- src/main/java/io/github/pitzzahh/libraryManagementSystem/controllers/StudentController.java
+++ src/main/java/io/github/pitzzahh/libraryManagementSystem/controllers/StudentController.java
@@ -1,11 +1,14 @@
 package io.github.pitzzahh.libraryManagementSystem.controllers;
 
 import static io.github.pitzzahh.libraryManagementSystem.util.Util.*;
+import io.github.pitzzahh.libraryManagementSystem.entity.Category;
 import io.github.pitzzahh.libraryManagementSystem.entity.Page;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
+import javafx.scene.Parent;
 import java.util.Optional;
 import javafx.fxml.FXML;
 
@@ -24,10 +27,19 @@ public class StudentController {
     private Button logout;
 
     @FXML
+    @SuppressWarnings({"unchecked"})
     public void onBorrowBook(ActionEvent actionEvent) {
         actionEvent.consume();
         setPage(Page.BORROW_BOOK);
         loadPage(actionEvent, "borrow_books_window");
+
+        Parent parent = getParent("borrow_books_window");
+
+        getTable(parent, "availableBooks")
+                .ifPresent(tableView -> {
+                    Optional<ChoiceBox<?>> choiceBox = getChoiceBox(parent, "choiceBox");
+                    choiceBox.ifPresent(objectChoiceBox -> setAvailableBooksData(tableView, (ChoiceBox<Category>) objectChoiceBox));
+                });
     }
 
     @FXML
