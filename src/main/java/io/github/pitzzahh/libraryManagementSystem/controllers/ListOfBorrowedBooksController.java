e569e0521ff209ade1317067afa441c160ac3493
pitzzahh <pitzzahh@gmail.com>
2022-12-04 14:59:19 +0800

    added some injectable fields

--- src/main/java/io/github/pitzzahh/libraryManagementSystem/controllers/ListOfBorrowedBooksController.java
+++ src/main/java/io/github/pitzzahh/libraryManagementSystem/controllers/ListOfBorrowedBooksController.java
@@ -1,5 +1,24 @@
 package io.github.pitzzahh.libraryManagementSystem.controllers;
 
-public class ListOfBorrowedBooksController {
+import io.github.pitzzahh.libraryManagementSystem.entity.Book;
+import javafx.scene.control.TableView;
+import javafx.scene.control.Button;
+import javafx.fxml.Initializable;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import java.net.URL;
 
+public class ListOfBorrowedBooksController implements Initializable {
+
+    @FXML
+    public Button separateWindowButton;
+
+
+    @FXML
+    public TableView<Book> table;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
