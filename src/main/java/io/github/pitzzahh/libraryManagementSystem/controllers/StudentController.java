3604d271a2deda3ce8274f0d094608eb5a99959d
pitzzahh <pitzzahh@gmail.com>
2022-12-05 10:05:53 +0800

    refactored loadPage method invocation arguments

--- src/main/java/io/github/pitzzahh/libraryManagementSystem/controllers/StudentController.java
+++ src/main/java/io/github/pitzzahh/libraryManagementSystem/controllers/StudentController.java
@@ -2,6 +2,7 @@ package io.github.pitzzahh.libraryManagementSystem.controllers;
 
 import static io.github.pitzzahh.libraryManagementSystem.util.Util.*;
 import io.github.pitzzahh.libraryManagementSystem.entity.Category;
+import io.github.pitzzahh.libraryManagementSystem.entity.Book;
 import io.github.pitzzahh.libraryManagementSystem.entity.Page;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableView;
@@ -31,7 +32,7 @@ public class StudentController {
     public void onBorrowBook(ActionEvent actionEvent) {
         actionEvent.consume();
         setPage(Page.BORROW_BOOK);
-        loadPage(actionEvent, "borrow_books_window");
+        loadPage("student_window", "borrow_books_window");
 
         Parent parent = getParent("borrow_books_window");
 
@@ -59,17 +60,17 @@ public class StudentController {
     }
 
     @FXML
-    @SuppressWarnings({"rawtypes"})
+    @SuppressWarnings({"unchecked"})
     public void onViewBooks(ActionEvent actionEvent) {
         actionEvent.consume();
         setPage(Page.VIEW_BORROWED_BOOKS);
-        loadPage(actionEvent, "list_of_borrowed_books_window");
+        loadPage("student_window", "list_of_borrowed_books_window");
         Optional<TableView<?>> table = getTable("list_of_borrowed_books_window", "table");
-        table.ifPresent(this::setItemsToViewBooksTable);
+        table.map(e ->  (TableView<Book> )e)
+                .ifPresent(this::setItemsToViewBooksTable);
     }
 
-    @SuppressWarnings({"rawtypes", "unchecked"})
-    private void setItemsToViewBooksTable(TableView t) {
+    private void setItemsToViewBooksTable(TableView<Book> t) {
         getBorrowedBooksDataSource().clear();
         getBorrowedBooksDataSource().addAll(getAllBorrowedBooks());
         initTableColumns(t, new String[]{"bookId", "title", "author", "category", "dateBorrowed", "dateReturned"});
